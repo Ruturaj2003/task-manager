9372739dfe8e1c4c736ad530ef97f1ff1cf6f57f
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-10-07 18:11:30 +0530

    Setup Basic Express Server

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Task Manager App')
+const express = require("express");
+
+const app = express();
+
+// Server Setup
+const port = 3000;
+app.listen(port, () => console.log(`Server Listening on Port ${port}`));
+
+
+// Routes 
+app.get('/hello',(req,res)=>{
+    res.send("Task Manager is Active")
+})
+
