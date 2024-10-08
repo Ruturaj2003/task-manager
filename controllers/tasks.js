d7779405c7376e108e7c4838a72a9ab65c532567
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-10-08 16:10:52 +0530

    Implement Get Single Task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,24 +1,38 @@
 const Task = require("../models/Task");
-
+//TODO: GET ALL TASKS
 const createTask = async (req, res) => {
   try {
     const task = await Task.create(req.body);
     res.status(201).json({ task });
-    
   } catch (error) {
     console.log(error);
     res.status(500).json({
-      msg:"Something went wrong in the server"
-    })
+      msg: "Something went wrong in the server",
+    });
   }
-  
+};
+
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params.id;
+    const task = await Task.findOne({
+      _id: taskID,
+    });
+    if (!task) {
+      return res.status(404).json({ msg: "Could not find the Task" });
+    }
+
+    res.staus(200).json({ task });
+  } catch (error) {
+    res.status(500).json({
+      msg: "Something went wrong in the server",
+    });
+    console.log("Could not find the task");
+  }
 };
 
 const getAllTasks = (req, res) => {
   res.send("All Items");
-};
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
 };
 const updateTask = (req, res) => {
   res.send("Update Item");
@@ -33,4 +47,4 @@ module.exports = {
   updateTask,
   deleteTask,
   createTask,
-}; 
+};
